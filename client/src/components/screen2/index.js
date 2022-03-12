622bd0712b14d717a508af6feaa38b2f630a0458
chitaranjan <chitabiswal4@gmail.com>
2022-03-12 12:59:58 +0530

    files select added

--- client/src/components/screen2/index.js
+++ client/src/components/screen2/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { TextField, Button } from "@mui/material";
 import axios from "axios";
 import RuntimeParameter from "./RuntimeParameter";
@@ -7,12 +7,18 @@ import EnvironmentVariable from "./EnvironmentVariable";
 const Screen2 = () => {
   const [rp, setRp] = useState([]);
   const [ev, setEv] = useState([]);
-  const [file, setFile] = useState("");
+  const [files, setFiles] = useState([]);
   const [result, setResult] = useState("");
 
   function handleChange(event) {
-    setFile(event.target.files[0].name);
+    const data = [];
+    for (let i = 0; i < event.target.files.length; i++) {
+      data.push(event.target.files[i].name);
+    }
+
+    setFiles(data);
   }
+
   const handleSubmit = async () => {
     const obj = {
       domain: "Docker_image",
@@ -20,12 +26,15 @@ const Screen2 = () => {
       flow2: "Environment Variables",
       atn1: rp,
       atn2: ev,
-      file: file,
+      files: files,
     };
+
     const res = await axios.post("http://localhost:9001/context", obj);
     setResult(res.data);
   };
-
+  const handleRemoveItem = (name) => {
+    setFiles(files.filter((item) => item !== name));
+  };
   return (
     <div className="screen-main-div">
       <h4 className="main-heading">Screen 2</h4>
@@ -42,8 +51,18 @@ const Screen2 = () => {
       <RuntimeParameter setRp={setRp} />
 
       <EnvironmentVariable setEv={setEv} />
+
       <div>
         <input type="file" multiple onChange={handleChange} />
+      </div>
+
+      <div>
+        {files.map((file, i) => (
+          <li key={i}>
+            {file}
+            <button onClick={() => handleRemoveItem(files[i])}> X</button>
+          </li>
+        ))}
       </div>
 
       <div className="screen-button">
--- server/routes/screen2/context.js
+++ server/routes/screen2/context.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const { exec } = require("child_process");
 router.post("/", async (req, res) => {
   try {
-    const { domain, flow1, flow2, atn1, atn2, file } = req.body;
+    const { domain, flow1, flow2, atn1, atn2, files } = req.body;
 
     const arr1 = [];
     const arr2 = [];
@@ -13,19 +13,23 @@ router.post("/", async (req, res) => {
       obj[`${i.context_entity}`] = i.value;
       arr1.push(obj);
     }
-    for (let i of atn2) {
+    for (let j of atn2) {
       const obj = {};
-      obj[`${i.context_entity}`] = i.value;
+      obj[`${j.context_entity}`] = j.value;
       arr2.push(obj);
     }
-    const path = `Workcontainer/Workspace/${file}`;
+    const arr3 = [];
 
-    fs.writeFileSync(path, "Demo text", function (err) {
-      console.log("File is created successfully.");
-    });
-    const obj2 = {};
-    obj2[`${file}`] = path;
-    const arr3 = [obj2];
+    for (let k of files) {
+      const obj2 = {};
+      const path = `Workcontainer/Workspace/${k}`;
+      obj2[`${k}`] = path;
+      arr3.push(obj2);
+      fs.writeFileSync(path, "Demo text", function (err) {
+        console.log("File is created successfully.");
+      });
+    }
+
     const context = [
       { domain: domain, flow: flow1, attentionentities: arr1 },
       { domain: domain, flow: flow2, attentionentities: arr2 },
